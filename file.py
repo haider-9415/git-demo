156cc84826f421732341ed614e4b4a511d5c6319
haider-9415 <116593197+haider-9415@users.noreply.github.com>
2022-10-25 14:22:33 +0530

    Add files via upload

---
+++ file.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+from matplotlib import rcParams as rc
+import pandas as pd
+import numpy as np
+subjects=
+marks=
+plt.bar(subjects,marks)
+plt.xlabel('Marks')
+plt.ylabel('Subjects')
+plt.title('Marksheet')
+plt.show()
